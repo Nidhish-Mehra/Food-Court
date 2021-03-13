391a8fe1ca5484c37aab5e4ff1d605cc62125cfd
Rahul Lanjewar <rahullanjewar93@gmail.com>
2021-03-13 15:50:58 +0530

    added: delete menu items

--- client/src/components/menu/addItems.js
+++ client/src/components/menu/addItems.js
@@ -3,48 +3,76 @@ import M from 'materialize-css'
 import { useHistory } from 'react-router-dom'
 
 function AddItems() {
-    const history = useHistory()
     const [menu,setMenu] = useState([])
     const [itemName,setItemName] = useState('')
     const [itemPrice,setItemPrice] = useState('')
 
     const getMenuItems =async ()=>{
        try{
-            const response = await fetch("/getMenuItems")
+            const response = await fetch('/getMenuItems')
             const data = await response.json()
             if(data.error){
-                M.toast({html:data.error, classes:"#c62828 red darken-3"})
+                M.toast({html:data.error, classes:'#c62828 red darken-3'})
             }
             else{
                 setMenu(data);
-                M.toast({html: "Retrived Menu Successfully", classes:"#43a047 green darken-1"})
+                M.toast({html: 'Retrived Menu Successfully', classes:'#43a047 green darken-1'})
             }
         }catch(error){
             console.log(error)
         }
     }
-    const createItem = ()=>{
-        fetch("/createMenuItem",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                itemName,
-                itemPrice
+
+    const createItem = async()=>{
+        try{
+            const res = await fetch('/createMenuItem',{
+                method:'post',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    itemName,
+                    itemPrice
+                })
             })
-        }).then(res=>res.json())
-        .then(data=>{
+            const data = await res.json()
             if(data.error){
-                M.toast({html:data.error, classes:"#c62828 red darken-3"})
+                M.toast({html:data.error, classes:'#c62828 red darken-3'})
             }
             else{
-                M.toast({html: data.message, classes:"#43a047 green darken-1"})
-                history.push('/')
+                setMenu((prevState)=>{
+                    return [...prevState,data]
+                })
+                console.log(menu)
+                M.toast({html: 'Menu Item Created Successfully', classes:'#43a047 green darken-1'})
             }
-        }).catch(error=>{
+        }catch(error){
             console.log(error)
-        })
+        }
+    }
+
+    const deleteMenuItem = async(itemName)=> {
+        try{
+            const res = await fetch('/deleteMenuItem',{
+                method:'post',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    itemName
+                })
+            })
+            const data = await res.json()
+            if(data.error){
+                M.toast({html:data.error, classes:'#c62828 red darken-3'})
+            }
+            else{
+                setMenu(data)
+                M.toast({html: 'Item Deleted Successfully', classes:'#43a047 green darken-1'})
+            }
+        }catch(error){
+            console.log(error)
+        }
     }
 
     useEffect(()=>{
@@ -54,32 +82,32 @@ function AddItems() {
     return (
         <>
         <div className='container'>
-        <div className="row">
-                <div className="row">
-                    <div className="input-field col s4">
-                        <label htmlFor="itemName">Item Name</label>
+        <div className='row'>
+                <div className='row'>
+                    <div className='input-field col s4'>
+                        <label htmlFor='itemName'>Item Name</label>
                         <input
-                            placeholder="name"
-                            type="text"
+                            placeholder='name'
+                            type='text'
                             value={itemName}
                             onChange={(e)=>{
                                 setItemName(e.target.value)
                                 }}
                             />
                     </div>
-                    <div className="input-field col s4">
-                        <label htmlFor="itemPrice">Item Price</label>
+                    <div className='input-field col s4'>
+                        <label htmlFor='itemPrice'>Item Price</label>
                         <input
-                            placeholder="name"
-                            type="text"
+                            placeholder='name'
+                            type='text'
                             value={itemPrice}
                             onChange={(e)=>{
                                 setItemPrice(e.target.value)
                                 }}
                             />
                     </div>
-                    <div className="input-field col s4">
-                    <button className="waves-effect waves-light btn large-btn"
+                    <div className='input-field col s4'>
+                    <button className='waves-effect waves-light btn large-btn'
                             onClick={()=>createItem()}
                             >
                                 Add
@@ -92,9 +120,22 @@ function AddItems() {
             <div className='row'>
             {menu.map((item)=>{
                 return(
-                    <div className='col s3'>
+                    <div className='col s4' key={item._id}>
                          <div className='card-panel' key={item._id}>
-                            <h5>{item.itemName} ₹{item.itemPrice} </h5>
+                            <div className='row'>
+                                <div className='col s6'>
+                                    <h6>{item.itemName} ₹{item.itemPrice} </h6>
+                                </div>
+
+                                <div className='col s6'>
+                                    <input
+                                        className='btn pink large-btn'
+                                        type='button'
+                                        name={item.itemName}
+                                        value='Delete'
+                                        onClick={(e)=>{deleteMenuItem(e.target.name)}}></input>
+                                </div>
+                            </div>
                     </div>
                     </div>
                 )
--- server/routes/menuItem.js
+++ server/routes/menuItem.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const requireSignin = require('../middleware/requireSignin')
 const menuItem = require('../models/menuItem')
-const { getMenuItems,createMenuItem } =require('../controllers/menuItems')
+const { getMenuItems,createMenuItem,deleteMenuItem } =require('../controllers/menuItems')
 
 router.get('/getMenuItems',getMenuItems)
 
 router.post('/createMenuItem',createMenuItem)
+router.post('/deleteMenuItem',deleteMenuItem)
 
 module.exports = router;
