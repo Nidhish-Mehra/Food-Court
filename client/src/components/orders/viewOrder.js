e627444d0f44dec081396e12c57ab2b4d48ce371
Rahul Lanjewar <rahullanjewar93@gmail.com>
2021-03-15 17:01:07 +0530

    going back to prev commit

--- client/src/components/orders/viewOrder.js
+++ client/src/components/orders/viewOrder.js
@@ -11,23 +11,19 @@ function ViewOrder() {
   const year = dateObj.getFullYear();
 
   const output = month  + '\n'+ day  + ',' + year;
+  const menu = [ {itemName:'sandwich',itemPrice:30},
+                {itemName:'coffee',itemPrice:20},
+                {itemName:'poha',itemPrice:20},
+                {itemName:'upma',itemPrice:20},
+                {itemName:'tea',itemPrice:10},
+                {itemName:'breadButterJam',itemPrice:20}
+              ]
 
-  let totalPrice=0 ;
-  let itemQuantity=0 ;
-  let itemPrice=0 ;
-  let itemTotal=0 ;
-  let finalTotal = 0
-  let finalQuantity = 0
-  let finalTotalToday = 0
-  let finalQuantityToday = 0
-
-  const [order,setOrder] = useState({
+  let [order,setOrder] = useState({
     todayOrders : [],
     tenMinuteOrders : [],
     prevWeekOrders : []
   })
-
-  const [menu,setMenu] = useState([])
 
   const getOrders = async()=>{
       try{
@@ -43,6 +39,7 @@ function ViewOrder() {
           M.toast({html:data.error, classes:"#c62828 red darken-3"})
         }
         else{
+          order= []
           setOrder({todayOrders : data.todayOrders,tenMinuteOrders : data.tenMinuteOrders,prevWeekOrders : data.prevWeekOrders})
           M.toast({html: "Retrived Orders Successfully", classes:"#43a047 green darken-1"})
         }
@@ -50,29 +47,30 @@ function ViewOrder() {
       catch(error){
         console.log(error)
       }
+
   }
 
-  const getMenuItems =async ()=>{
-    try{
-         const response = await fetch('/getMenuItems')
-         const data = await response.json()
-         if(data.error){
-             M.toast({html:data.error, classes:'#c62828 red darken-3'})
-         }
-         else{
-            const newMenu  = data.map((item) => item.itemName)
-            setMenu(newMenu);
-            M.toast({html: 'Retrived Menu Items Successfully', classes:'#43a047 green darken-1'})
-         }
-     }catch(error){
-         console.log(error)
-     }
- }
-
   useEffect(()=>{
-    getOrders()
-    getMenuItems()
+    getOrders();
   },[])
+
+  let todayOrdersData = [];
+
+  if(order.todayOrders.length>0){
+    todayOrdersData = order.todayOrders.map((singleOrder)=>{
+      return(
+        <tr>
+          <td>{singleOrder.orderedBy}</td>
+          <td>{singleOrder.poha || singleOrder.upma || singleOrder.tea || singleOrder.coffee || singleOrder.breadButterJam || singleOrder.sandwich }</td>
+          <td>{singleOrder.orderedBy}</td>
+          <td>{singleOrder.orderedBy}</td>
+          <td>{singleOrder.orderedBy}</td>
+        </tr>
+      )
+    })
+  }
+
+  console.log(todayOrdersData)
 
   return (
       <>
@@ -85,49 +83,15 @@ function ViewOrder() {
                 <table className='striped responsive'>
                   <thead>
                     <tr>
-                      <th>Item Name</th>
                       <th>Customer Name</th>
+                      <th>Item Name</th>
                       <th>Item Quantity</th>
                       <th>Item Price</th>
                       <th>Total</th>
                     </tr>
                   </thead>
-
                   <tbody>
-
-                  {menu.map((menuItem)=>{
-                    finalTotalToday = finalTotalToday + itemTotal
-                    finalQuantityToday = finalQuantityToday + itemQuantity
-                    itemQuantity=0;
-                    itemPrice=0;
-                    itemTotal=0;
-                    return(
-                      <tr key={Date.now()}>
-                        <td>{menuItem}</td>
-                          {order.tenMinuteOrders.map((item)=>{
-                            item.orderDetails.map((component)=>{
-                              if(component.itemName == menuItem){
-                                itemQuantity = itemQuantity + component.itemQuantity
-                                itemPrice = itemPrice + component.itemPrice
-                                itemTotal = itemQuantity*itemPrice
-                              }
-                            });
-                            return(<td>{item.orderedBy}</td>)
-                          })
-                          }
-                        <td>{itemQuantity}</td>
-                        <td>{itemPrice}</td>
-                        <td>{itemTotal}</td>
-                      </tr>
-                    )
-                  })}
-                    <tr>
-                      <td>Total </td>
-                      <td></td>
-                      <td>{finalQuantityToday+itemQuantity}</td>
-                      <td></td>
-                      <td>{finalTotalToday+itemTotal}</td>
-                    </tr>
+                    {todayOrdersData}
                   </tbody>
                 </table>
               </div>
@@ -145,40 +109,8 @@ function ViewOrder() {
                       <th>Total</th>
                     </tr>
                   </thead>
-
                   <tbody>
 
-                  {menu.map((menuItem)=>{
-                    finalTotalToday = finalTotalToday + itemTotal
-                    finalQuantityToday = finalQuantityToday + itemQuantity
-                    itemQuantity=0;
-                    itemPrice=0;
-                    itemTotal=0;
-                    return(
-                      <tr key={Date.now()}>
-                        <td>{menuItem}</td>
-                          {order.todayOrders.map((item)=>{
-                            item.orderDetails.map((component)=>{
-                              if(component.itemName == menuItem){
-                                itemQuantity = itemQuantity + component.itemQuantity
-                                itemPrice = itemPrice + component.itemPrice
-                                itemTotal = itemQuantity*itemPrice
-                              }
-                            })
-                          })
-                          }
-                        <td>{itemQuantity}</td>
-                        <td>{itemPrice}</td>
-                        <td>{itemTotal}</td>
-                      </tr>
-                    )
-                  })}
-                    <tr>
-                      <td>Total </td>
-                      <td>{finalQuantityToday+itemQuantity}</td>
-                      <td></td>
-                      <td>{finalTotalToday+itemTotal}</td>
-                    </tr>
                   </tbody>
                 </table>
             </div>
@@ -196,39 +128,8 @@ function ViewOrder() {
                       <th>Total</th>
                     </tr>
                   </thead>
-
                   <tbody>
-                  {menu.map((menuItem)=>{
-                    finalTotal = finalTotal + itemTotal
-                    finalQuantity = finalQuantity + itemQuantity
-                    itemQuantity=0;
-                    itemPrice=0;
-                    itemTotal=0;
-                    return(
-                      <tr key={Date.now()}>
-                        <td>{menuItem}</td>
-                          {order.prevWeekOrders.map((item)=>{
-                            item.orderDetails.map((component)=>{
-                              if(component.itemName == menuItem){
-                                itemQuantity = itemQuantity + component.itemQuantity
-                                itemPrice = itemPrice + component.itemPrice
-                                itemTotal = itemQuantity*itemPrice
-                              }
-                            })
-                          })
-                          }
-                        <td>{itemQuantity}</td>
-                        <td>{itemPrice}</td>
-                        <td>{itemTotal}</td>
-                      </tr>
-                    )
-                  })}
-                    <tr>
-                      <td>Total </td>
-                      <td>{finalQuantity+itemQuantity}</td>
-                      <td></td>
-                      <td>{finalTotal+itemTotal}</td>
-                    </tr>
+
                   </tbody>
                 </table>
             </div>
